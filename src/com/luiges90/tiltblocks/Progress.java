7bba27dd4aee07bb5bd0660e567c2d123ca98a0f
Peter WONG <luiges90@gmail.com>
2014-02-05 18:53:14 +0800

    fix possible file leak

--- src/com/luiges90/tiltblocks/Progress.java
+++ src/com/luiges90/tiltblocks/Progress.java
@@ -7,25 +7,26 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import com.google.analytics.tracking.android.EasyTracker;
-import com.google.analytics.tracking.android.MapBuilder;
-
 import android.content.Context;
 import android.util.Log;
+
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.MapBuilder;
 
 public class Progress {
 
     public static final String PROGRESS_FILE = "tiltblocks_progress";
-    
+
     private int currentLevel;
     private int storedProgress;
     private Context context;
-    
+
     private File getFile() {
         return new File(context.getFilesDir(), PROGRESS_FILE);
     }
-    
+
     private static Progress instance;
+
     public static Progress instance(Context context) {
         if (instance == null) {
             instance = new Progress(context);
@@ -36,34 +37,53 @@ public class Progress {
     private Progress(Context context) {
         this.context = context;
         File file = getFile();
-        
+
+        BufferedReader br = null;
         try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            currentLevel = Integer.parseInt(br.readLine());
-            br.close();
+            try {
+                br = new BufferedReader(new FileReader(file));
+                currentLevel = Integer.parseInt(br.readLine());
+                br.close();
+            } catch (IOException ex) {
+                EasyTracker.getInstance(context).send(
+                        MapBuilder.createEvent("progress", "status", "no progress found", null)
+                                .build());
+                currentLevel = 0;
+                saveProgress();
+                if (br != null) {
+                    br.close();
+                }
+            } catch (NumberFormatException ex) {
+                EasyTracker.getInstance(context).send(
+                        MapBuilder.createEvent("progress", "status", "no progress found", null)
+                                .build());
+                currentLevel = 0;
+                saveProgress();
+                if (br != null) {
+                    br.close();
+                }
+            }
         } catch (IOException ex) {
-            EasyTracker.getInstance(context).send(
-                    MapBuilder.createEvent("progress", "status", "no progress found", null)
-                        .build());
-            currentLevel = 0;
-            saveProgress();
-        } catch (NumberFormatException ex) {
-            EasyTracker.getInstance(context).send(
-                    MapBuilder.createEvent("progress", "status", "no progress found", null)
-                        .build());
-            currentLevel = 0;
-            saveProgress();
+            Log.e(GameActivity.LOG_TAG, "Unable to close file", ex);
         }
     }
-    
+
     private void saveProgress() {
+        BufferedWriter bw = null;
         try {
             File file = getFile();
-            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            bw = new BufferedWriter(new FileWriter(file));
             bw.write(Integer.toString(currentLevel));
             bw.close();
         } catch (IOException e) {
             Log.e(GameActivity.LOG_TAG, "Unable to save progress", e);
+            try {
+                if (bw != null) {
+                    bw.close();
+                }
+            } catch (IOException ex) {
+                Log.e(GameActivity.LOG_TAG, "Unable to close file", ex);
+            }
         }
     }
 
@@ -74,27 +94,28 @@ public class Progress {
     public void advanceLevel(int newLevel) {
         if (newLevel > currentLevel) {
             EasyTracker.getInstance(context).send(
-                    MapBuilder.createEvent("progress", "progress", "reached level " + newLevel, null)
-                        .build());
+                    MapBuilder.createEvent("progress", "progress", "reached level " + newLevel,
+                            null)
+                            .build());
             currentLevel = newLevel;
             saveProgress();
         }
     }
-    
+
     public boolean clearProgress() {
         EasyTracker.getInstance(context).send(
                 MapBuilder.createEvent("progress", "progress", " progress cleared", null)
-                    .build());
+                        .build());
         storedProgress = currentLevel;
         currentLevel = 0;
         File file = getFile();
         return file.delete();
     }
-    
+
     public void restoreProgress() {
         EasyTracker.getInstance(context).send(
                 MapBuilder.createEvent("progress", "progress", " progress restored", null)
-                    .build());
+                        .build());
         currentLevel = storedProgress;
         saveProgress();
     }
